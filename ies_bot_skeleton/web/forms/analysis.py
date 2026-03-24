b36fc74c26ab3ee1ec053462ad4d36962cd7b444
danya <danny.glazovs@gmail.com>
2026-03-24 10:02:09 +0500

    fixing errors

--- ies_bot_skeleton/web/forms/analysis.py
+++ ies_bot_skeleton/web/forms/analysis.py
@@ -4,15 +4,13 @@ from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
 from wtforms.validators import Optional
 
-from ..services.test_game_preset import TEST_GAME_BUNDLED_FORECAST_NAME
-
 
 class ForecastSelectionForm(FlaskForm):
     selected_forecast_id = SelectField(
         "Активный прогноз",
         coerce=int,
         validators=[Optional()],
-        choices=[(0, TEST_GAME_BUNDLED_FORECAST_NAME)],
-        default=0,
+        choices=[],
+        default=None,
     )
     submit = SubmitField("Выбрать прогноз")
